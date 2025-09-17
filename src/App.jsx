b58c0c7f0e1b24929a748fe8a3040f554f0f01e5
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-09-17 10:28:10 +0100

    feat: configure routes for page to page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import CountryDetails from "./pages/CountryDetails";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/:country", element: <CountryDetails /> },
+    ],
+  },
+]);
 function App() {
-  return <main></main>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
